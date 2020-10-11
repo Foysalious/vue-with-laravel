bc741d6f672ac0bc230d5b49dff14a3dfd58f5b2
foysalious <foysalrahman112@gmail.com>
2020-10-11 16:19:18 +0600

    update

---
+++ resources/js/app.js
@@ -0,0 +1,23 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+require('./bootstrap');
+
+window.Vue = require('vue');
+import CreateComponent from './components/CreateComponent'
+
+
+
+
+
+
+const app = new Vue({
+    el: '#app',
+    render: h => h(CreateComponent),
+    components: {
+        CreateComponent
+    }
+});
---
+++ resources/js/components/CreateComponent.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="card">
+          <div class="card-header">Create New Student</div>
+
+          <div class="card-body">
+            <form>
+              <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input
+                  type="email"
+                  v-model="email"
+                  class="form-control"
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                  placeholder="Enter email"
+                />
+                <small id="emailHelp" class="form-text text-muted"
+                  >We'll never share your email with anyone else.</small
+                >
+              </div>
+
+               <div class="form-group">
+                <label for="name">Name</label>
+                <input
+                  type="text"
+                  v-model="name"
+                  class="form-control"
+                  id="name"
+                  aria-describedby="emailHelp"
+                  placeholder="Enter name"
+                />
+                
+              </div>
+
+              <div class="form-group">
+                <label for="phone">Phone</label>
+                <input
+                  type="text"
+                  v-model="phone"
+                  class="form-control"
+                  id="phone"
+                  aria-describedby="emailHelp"
+                  placeholder="Enter phone number"
+                />
+                
+              </div>
+              
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            name:'',
+            email:'',
+            phone:''
+
+        }
+    },
+  mounted() {
+    console.log("Component mounted.");
+  },
+};
+</script>
